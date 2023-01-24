5b69578455e5dd43e97557292f2571fc97c09dc9
sxnvte <112719749+sxnvte@users.noreply.github.com>
2023-01-24 20:14:21 +0100

    Create watermark.lua

---
+++ watermark.lua
@@ -0,0 +1,25 @@
+-- Gui to Lua
+-- Version: 3.2
+
+-- Instances:
+
+local ScreenGui = Instance.new("ScreenGui")
+local TextLabel = Instance.new("TextLabel")
+
+--Properties:
+
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+TextLabel.Parent = ScreenGui
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.Position = UDim2.new(0, 0, 0.950617313, 0)
+TextLabel.Size = UDim2.new(0, 143, 0, 40)
+TextLabel.Font = Enum.Font.RobotoMono
+TextLabel.Text = "oxygen.nigg"
+TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.TextSize = 23.000
+
+
+-- niggers
